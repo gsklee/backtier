e5eee12a97e92a52ed584bfa46c0e5cc7220ddf9
G. Kay Lee <balancetraveller+github@gmail.com>
2015-12-30 15:45:03 +0800

    Save the result of the interactive prompt

--- commands/init.js
+++ commands/init.js
@@ -1,29 +1,53 @@
-import vorpal from 'vorpal';
+import path from 'path';
 
-const cli = vorpal();
+import {assign} from 'bound-native-methods/object';
+import Vorpal from 'vorpal';
+import JSONFilePlus from 'json-file-plus';
+
+const cli = Vorpal();
 
 cli.command('init', 'Initializes this project.')
    .action(function (args, callback) {
-     this.log();
+     const {log, prompt} = this;
 
-     this.prompt([{
-       name: 'name',
-       message: 'Enter the package name: '
-     }, {
-       name: 'description',
-       message: 'Enter the package description: '
-     }, {
-       name: 'url',
-       message: 'Enter the URL of the repository: '
-     }, {
-       type: 'confirm',
-       name: 'isPrivate',
-       message: 'Is the project private?'
-     }], answers => {
-       this.log();
+     log();
 
-       callback();
-     });
+     prompt(
+       [{
+         name: 'name',
+         message: 'Enter the package name: '
+       }, {
+         name: 'description',
+         message: 'Enter the package description: '
+       }, {
+         name: 'url',
+         message: 'Enter the URL of the repository: '
+       }, {
+         type: 'confirm',
+         name: 'private',
+         message: 'Is the project private?'
+       }],
+
+       async ({url, ...answers}) => {
+         const file = await JSONFilePlus(path.join(process.cwd(), 'package.json'));
+
+         file.set(answers::assign({
+           repository: {url}
+         }));
+
+         log();
+
+         try {
+           await file.save(callback);
+
+           log('Success!');
+         } catch (error) {
+           log('Error!', error);
+         }
+
+         log();
+       }
+     );
    });
 
 cli.delimiter('backtier>').show().exec('init');
